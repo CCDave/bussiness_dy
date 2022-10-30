c3314afdd7c2b390115e4ee3672fc65e22398826
wangzhiwei <wangzhiwei@taokedawanjia.com>
2022-10-30 17:38:19 +0800

    add

--- orders/test_order_table.js
+++ orders/test_order_table.js
@@ -115,7 +115,7 @@ let query_set = {
   groupByFieldList: [ids.r_order_status],
   aggregationQueryList: [
     { field: ids.order_id, func: "count", distinct: false },
-    { field: ids.p_count, func: "count", distinct: false },
+    { field: ids.p_count, func: "sum", distinct: false },
     { field: ids.settle_amount, func: "sum", distinct: false },
     { field: ids.total_income, func: "sum", distinct: false },
     { field: ids.platform_service_amount, func: "sum", distinct: false },
@@ -147,60 +147,89 @@ var resp = informat.table.query(ids.table_id, query_set);
 "订单应付金额_求和"
 "收入合计_求和"
 "手续费_求和"
-"商品数量_数量"
+"商品数量_求和"
 "平台服务费_求和"
 "支出合计_求和"
 */
 var datas = resp.list;
-
 function getValueByPairs(collect, c_key, c_value, value) {
   return collect.sum((product) =>
     product[c_key] == c_value ? product[value] : 0
   );
 }
-
+function getValues(collect, c_key, c_vale, p_total) {
+  var obj = {
+    order_count: 0,
+    need_pay_mount: 0,
+    p_count: 0,
+    settle_amount: 0,
+    percent: 0,
+  };
+  obj.order_count = getValueByPairs(collect, c_key, c_vale, "子订单编号_数量");
+  obj.need_pay_mount = getValueByPairs(
+    collect,
+    c_key,
+    c_vale,
+    "订单应付金额_求和"
+  );
+  obj.p_count = getValueByPairs(collect, c_key, c_vale, "商品数量_求和");
+  obj.settle_amount = getValueByPairs(collect, c_key, c_vale, "结算金额_求和");
+  obj.percent = ((obj.order_count / p_total) * 100).toFixed(2);
+  return obj;
+}
 const collect_s = collect(datas);
 let s = collect_s;
 var total_orders = s.sum("子订单编号_数量");
-var no_pay_orders = getValueByPairs(s, "订单阶段", "未付款", "子订单编号_数量");
-var refund_before = getValueByPairs(
-  s,
-  "订单阶段",
-  "发货前退款",
-  "子订单编号_数量"
+
+var no_pay_orders = getValues(s, "订单阶段", "未付款", total_orders);
+
+var pay_count = total_orders - no_pay_orders.order_count;
+var pay_percent = (
+  100 -
+  (no_pay_orders.order_count / total_orders) * 100
+).toFixed(2);
+
+var refund_before = getValues(s, "订单阶段", "发货前退款", pay_count);
+var refund_after = getValues(s, "订单阶段", "发货后退款", pay_count);
+
+var total_refund = refund_after.order_count + refund_before.order_count;
+var total_refund_percent = ((total_refund / pay_count) * 100).toFixed(2);
+
+var delivery = getValues(s, "订单阶段", "已发货", pay_count);
+
+var prepare = getValues(s, "订单阶段", "备货中", pay_count);
+
+var waite_settle = delivery.order_count + prepare.order_count;
+var waite_settle_p_count = delivery.p_count + prepare.p_count;
+var waite_settle_percent = ((waite_settle / pay_count) * 100).toFixed(2);
+var waite_settle_mount = delivery.need_pay_mount + prepare.need_pay_mount;
+
+var settle = getValues(s, "订单阶段", "已结算", pay_count);
+
+var r_settle = getValues(s, "订单阶段", "反结算", pay_count);
+
+var platform_service_amount = 0;
+datas.filter(
+  (f) =>
+    (platform_service_amount += f["平台服务费_求和"] ? f["平台服务费_求和"] : 0)
 );
-var refund_after = getValueByPairs(
-  s,
-  "订单阶段",
-  "发货后退款",
-  "子订单编号_数量"
-);
-var delivery = getValueByPairs(s, "订单阶段", "已发货", "子订单编号_数量");
-var delivery_amount = getValueByPairs(
-  s,
-  "订单阶段",
-  "已发货",
-  "订单应付金额_求和"
-);
-var prepare = getValueByPairs(s, "订单阶段", "备货中", "子订单编号_数量");
-var prepare_amount = getValueByPairs(
-  s,
-  "订单阶段",
-  "备货中",
-  "订单应付金额_求和"
-);
-var pay_count = total_orders - no_pay_orders;
-var total_refund = refund_after + refund_before;
-var waite_settle = delivery + prepare;
-var waite_settle_mount = delivery_amount + prepare_amount;
-var pay_percent = (1 - no_pay_orders / total_orders).toFixed(2);
-var refund_percent = (total_refund / pay_count).toFixed(2);
-var waite_settle_percent = (waite_settle / pay_count).toFixed(2);
-var before_percent = (refund_before / pay_count).toFixed(2);
-var after_percent = (refund_after / pay_count).toFixed(2);
+
 var content =
-  "|渠道|订单数|未付款|付款率|发前退款|发前退款率|发后退款|发后退款率|未支付/退款|退款率|已发货|备货中|待结算|待结算比例|待结算金额\n" +
-  "|---|------|----- |-----|-------|---------|--------|---------|----------|------|-----|------|-----|--------|--------| \n";
-content += `|总数| ${total_orders}|${no_pay_orders}|${pay_percent}|${refund_before}|${before_percent}|${refund_after}|${after_percent}|${total_refund}|${refund_percent}|${delivery}|${prepare} |${waite_settle} |${waite_settle_percent}|${waite_settle_mount} |\n`;
+  "|渠道|订单|付款|付款率|前退|前退率|后退|\
+  后退率|退款|退款率|发货|发货件|发货率|发货额|\
+  备货|备货件|备货率|备货额|待结算|待结件|待结算率|\
+  待结算额|结算|结算件|结算率|结算额|反结算|反结算件|\
+  反结算率|反结算额|平台服务费|\n" +
+  "|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|\n";
+content += `|总数| ${total_orders}|${pay_count}|${pay_percent}|\
+${refund_before.order_count}|${refund_before.percent}|\
+${refund_after.order_count}|${refund_after.percent}|\
+${total_refund}|${total_refund_percent}|\
+${delivery.order_count}|${delivery.p_count} |${delivery.percent} |${delivery.need_pay_mount} |\
+${prepare.order_count}|${prepare.p_count} |${prepare.percent} |${prepare.need_pay_mount} |\
+${waite_settle}|${waite_settle_p_count} |${waite_settle_percent} |${waite_settle_mount} |\
+${settle.order_count}|${settle.p_count} |${settle.percent} |${settle.need_pay_mount} |\
+${r_settle.order_count}|${r_settle.p_count} |${r_settle.percent} |${r_settle.need_pay_mount} |\
+${platform_service_amount}|\n`;
 
 dashboard.setMarkdownOption(content);
